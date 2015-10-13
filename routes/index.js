877b25a587ad40ffe0daeb82929eb5cb222d85c5
MikeMeissner <tennisapb@gmail.com>
2015-10-13 10:52:33 -0400

    added all views

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Express' });
+});
+
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
  
